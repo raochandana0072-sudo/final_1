cba246c79bda45387abaf74d8166657e495409b8
vamsi <vamsikrishna11998@gmail.com>
2026-01-05 21:28:21 +0530

    Add allowedHosts for Render

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,9 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
+  preview: {
+    allowedHosts: ['final-1-z55x.onrender.com'],
+  },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
